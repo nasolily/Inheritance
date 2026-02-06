4321b8a8285d1ca845c334f01964591f768cdeeb
nadi <nadiaslcowins@gmail.com>
2026-02-06 17:38:59 -0500

    Added SalaryWorker overrides, created SalaryWorkerTest

--- src/SalaryWorker.java
+++ src/SalaryWorker.java
@@ -1,8 +1,22 @@
 public class SalaryWorker extends Worker {
-    double annualSalary;
+    private double annualSalary;
 
-    public SalaryWorker(String john, String doe, String number, String engineer, int i, double v) {
-        super(john, doe, number, engineer, i, v);
-        this.annualSalary = v;
+    public SalaryWorker(String firstName, String lastName, String ID, String title, int YOB, double annualSalary) {
+        super(firstName, lastName, ID, title, YOB, annualSalary / 2080);
+        this.annualSalary = annualSalary;
     }
+
+    @Override
+    public double calculateWeeklyPay(double hoursWorked) {
+        return annualSalary / 52;
+    }
+
+    @Override
+    public void displayWeeklyPay(double hoursWorked) {
+        double weeklyPay = annualSalary / 52;
+        System.out.printf("Weekly Pay: $%.2f (Fraction of salary $%.2f)%n", weeklyPay, annualSalary);
+    }
+
+    @Override
+    public String toCSVDataString() { return super.toCSVDataString() + "," + annualSalary; }
 }
---
+++ src/SalaryWorkerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalaryWorkerTest {
+
+    @Test
+    void calculateWeeklyPay() {
+    }
+
+    @Test
+    void displayWeeklyPay() {
+    }
+
+    @Test
+    void toCSVDataString() {
+    }
+}
